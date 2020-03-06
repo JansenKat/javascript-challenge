bd0b269a6cdae8b3e3b0a2f3ca22ee640b7dc943
Kat Jansen <kat@17cosmicln.net>
2020-03-05 20:11:18 -0600

    Copy filter from UFO-level-1, add inputs and filters.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,29 @@
-// from data.js
-const tableData = data;
+const dateInput = d3.select("input #datetime")
+const cityInput = d3.select("input #city")
+const stateInput = d3.select("input #state")
+const countryInput = d3.select("input #country")
+const shapeInput = d3.select("input #shape")
 
-// YOUR CODE HERE!
+const button = d3.select("button")
+const handler = function(){
+
+    const tbody = d3.select("tbody")
+  
+    data.filter(dataRow => dataRow.datetime === dateInput.property('value'))\
+        .filter(dataRow => dataRow.city === cityInput.property('value'))\
+        .filter(dataRow => dataRow.state === stateInput.property('value'))\
+        .filter(dataRow => dataRow.country === countryInput.property('value'))\
+        .filter(dataRow => dataRow.shape === shapeInput.property('value'))
+        .forEach(dataRow => {
+            let row = tbody.append("tr")
+            row.append("td").text(dataRow.datetime)
+            row.append("td").text(dataRow.city)
+            row.append("td").text(dataRow.state)
+            row.append("td").text(dataRow.country)
+            row.append("td").text(dataRow.shape)
+            row.append("td").text(dataRow.durationMinutes)
+            row.append("td").text(dataRow.comments)
+        })
+  }
+
+  button.on("click", handler)
