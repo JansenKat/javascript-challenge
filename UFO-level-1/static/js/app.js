bd1a08ae58fc13c5ec89913c6e3ccaec7bc4ded8
Kat Jansen <kat@17cosmicln.net>
2020-03-05 18:00:22 -0600

    Create handler function to create table and filter based on filter condition.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,40 @@
 // from data.js
 const tableData = data;
 
+const input = d3.select("input")
+const button = d3.select("button")
+const table = d3.select("body").append("table")
+
 // YOUR CODE HERE!
+const handler = function(){
+
+    let filterCond = input.property("value")
+  
+    table.html("")
+    
+    const headerRow = table.append("thead").append("tr")
+    headerRow.append("th").text("date/time")
+    headerRow.append("th").text("city")
+    headerRow.append("th").text("state")
+    headerRow.append("th").text("country")
+    headerRow.append("th").text("shape")
+    headerRow.append("th").text("duration")
+    headerRow.append("th").text("comment")
+  
+    const tbody = table.append("tbody")
+  
+    data.filter(dataRow => dataRow.datetime === filterCond).forEach(dataRow => {
+      let row = tbody.append("tr")
+      row.append("td").text(dataRow.datetime)
+      row.append("td").text(dataRow.city)
+      row.append("td").text(dataRow.state)
+      row.append("td").text(dataRow.country)
+      row.append("td").text(dataRow.shape)
+      row.append("td").text(dataRow.durationMinutes)
+      row.append("td").text(dataRow.comments)
+    })
+  }
+
+
+  input.on("change", handler)
+  button.on("click", handler)
